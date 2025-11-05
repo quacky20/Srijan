9e6c67ff8a796e85c346d7ce39b958b2433c524c
bansalbhumi22 <24je0099@iitism.ac.in>
2025-11-05 17:25:03 +0530

    --

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './Hero.css';
 import logo from '../../assets/logo.png.png'; 
 import CountdownTimer from '../CountdownTimer/CountdownTimer';
+import Mandala from '../../assets/mandala.png';
+
 
 const Hero = () => {
   const targetDate = new Date('2026-01-16T00:00:00');
 
   return (
     <div className="hero-container">
-      
+   
+
+      <img src={Mandala} alt="Mandala Background Art" className="hero-mandala" />
+      <div className="mandala-layer"></div>
       <div className="hero-logo-container">
         <img src={logo} alt="Srijan Logo" className="hero-logo" />
       </div>
