06d7a12c7d4795994dd2f439e5a81d09003a8604
Harsh-Kumar-26 <harshkumar010377@gmail.com>
2025-11-05 16:32:48 +0530

    Initial be setup

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,58 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
-
 dotenv.config();
 
-connectDB();
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import rateLimit from 'express-rate-limit';
+import cookieParser from 'cookie-parser';
+import userrouter from './routes/userRoutes.js';
+import  errorHandler  from './middleware/errorMiddleware.js';
+
+
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin:process.env.CORS_ORIGIN || "*",
+  credentials:true
+}));
+
+
+app.use(express.json({limit: "16kb"}));
+
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Srijan API' });
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100,
+    message: "Too many requests from this IP, please try again later."
+  }));
+  app.use(express.static("public"));
+  app.use(express.urlencoded({extended:true , limit: "16kb"}));
+  app.use(cookieParser());
+  
+  app.use(errorHandler);
+  
+  // Routes
+  app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to Srijan API' });
+  });
+  
+  app.use("/api/v1/user",userrouter);
+  
+  app.use((req, res) => {
+    res.status(404).json({ message: "Not Found" });
+  });
+  
+  
+  const PORT = process.env.PORT || 5000;
+  
+  connectDB()
+  .then(()=>{
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }).catch((error)=>{
+    console.error("Failed to connect to the database", error);
+  });
---
+++ backend/models/UserModel.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userschema=new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:[true,"Full Name is required"],
+        trim:true
+    },
+    email:{
+        type:String,
+        required:[true,"Email id is required"],
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+        minlength:[8,"Password should atleast be 8 characters long"],
+        validate: {
+    validator: function (val) {
+      return /[!@#$%^&*(),.?":{}|<>]/.test(val);
+    },
+    message: "Password must contain at least one special character"
+    }
+},
+    mobilenumber:{
+        type:String,
+        required:[true,"Mobile Number is required"],
+        trim:true
+    },
+    ism_member:{
+        type:Boolean,
+        required:[true,"Is Member is required"],
+    },
+    refreshtoken:{
+        type:String
+    }
+},{timestamps:true});
+
+userschema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password=await bcrypt.hash(this.password,9);
+    }
+    next();
+});
+
+
+userschema.methods.isPasswordCorrect=async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+userschema.methods.generateaccesstoken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userschema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User= mongoose.model("User",userschema);
