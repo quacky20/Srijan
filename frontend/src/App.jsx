54cd024ebf86c8159f06368ef0ba74edec7bf1a0
dev-beast-451 <24mt0512@iitism.ac.in>
2025-11-09 15:56:57 +0530

    nits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import GalleryPage from "./pages/Gallery.jsx";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import GalleryPage from "/pages/Gallery";
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <Navbar />
+      <Hero />
       <GalleryPage />
-    </>
+      {}
+    </div>
   );
 }
+
+export default App;
